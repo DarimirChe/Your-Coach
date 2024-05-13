f8679bd73fdd19cc0cd8f712434601fac90d1dde
DarimirChe <darimrulit@gmail.com>
2024-05-13 20:08:19 +0300

    кнопка и список

--- app/src/main/java/com/example/yourcoach/MainActivity.java
+++ app/src/main/java/com/example/yourcoach/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.yourcoach;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button add_button = findViewById(R.id.add_button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a = 100;
+
+        add_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Обработка нажатия
+
+            }
+        });
     }
+
 }
